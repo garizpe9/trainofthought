a92616f83c152bd98520b3d0358d9f4ffbb0d4e7
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-11-23 18:55:44 -0600

    updating to include passport and passport seed

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -14,8 +14,27 @@ const journalSeed = [
 
 ];
 
+const userSeed = [
+  {
+    username: "username",
+    password: "password",
+  },
+
+];
+
 db.Journal.remove({})
   .then(() => db.Journal.collection.insertMany(journalSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+  db.AUser.insertMany(userSeed)
+  
   .then(data => {
     console.log(data.result.n + " records inserted!");
     process.exit(0);
