15f43c00877d68c48b6dadb61613adeed457279c
Amor Andal <aandal77@gmail.com>
2020-11-24 16:23:36 -0600

    added ui: navbar and jumbotron; modified related css files

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //import logo from "./logo.svg";
 import "./App.css";
+import Jumbotron from "./component/jumbotron/Jumbotron";
 import LandingPage from "./component/LandingPage";
-import NavBar from "./component/nav/Navbar"
+import NavBar from "./component/nav/NavBar"
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Router>
         <NavBar/>
+        <Jumbotron/>
         <LandingPage/>
         </Router>
        
