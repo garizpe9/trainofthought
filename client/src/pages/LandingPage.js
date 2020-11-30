ab2deaf9b13b34d82a9e21b6a90dbb4ead1277ef
Angel Alexander <angale22@gmail.com>
2020-11-30 11:37:44 -0600

    adding facerec page, installs and components

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import "../../App.css";
-import JournalCard from "../../component/journal/JournalCard";
-import JournalEntry from "../../component/journal/JournalEntry";
-import Jumbotron from "../../component/jumbotron/Jumbotron";
-import LandingPage from "../../component/LandingPage";
-import NavBar from "../../component/nav/Navbar"
+import "../App.css"
+import JournalCard from "../component/journal/JournalCard";
+import JournalEntry from "../component/journal/JournalEntry";
+import Jumbotron from "../component/jumbotron/Jumbotron";
+import LandingPage from "../component/LandingPage"
+import NavBar from "../component/nav/Navbar"
 
 class HomeLandingPage extends Component {
   render() {
