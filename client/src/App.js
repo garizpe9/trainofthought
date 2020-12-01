82d3a3afb01f8484c1ba2fd6dfe7dd52f30bb641
Giovanna Arizpe <veliaarizpe@gmail.com>
2020-11-30 20:44:40 -0600

    updated to include passport pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,9 @@ import "./App.css";
 import HomeLandingPage from "./pages/LandingPage"
 import NavBar from "./component/nav/Navbar"
 import FaceDetectionPage from './pages/FaceDetectionPage/FaceDetectionPage';
+import Login from './pages/Passport/login';
+import Home from './pages/Passport/home';
+import Register from './pages/Passport/register';
 
 function App() {
   return (
@@ -16,6 +19,15 @@ function App() {
           </Route>
           <Route exact path={"/facerec"}>
             <FaceDetectionPage />
+          </Route>
+          <Route exact path={"/login"}>
+            <Login />
+          </Route>
+          <Route exact path={"/home"}>
+            <Home />
+          </Route>
+          <Route exact path={"/register"}>
+            <Register />
           </Route>
         </Switch>
       </div>
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
+var auth = require("../controllers/AuthController.js");
 
 // API Routes
 router.use("/api", apiRoutes);
@@ -9,5 +10,25 @@ router.use("/api", apiRoutes);
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
+
+
+//Passport gets and posts
+router.get('/', auth.home);
+
+// route to register page
+router.get('/register', auth.register);
+
+// route for register action
+router.post('/register', auth.doRegister);
+
+// route to login page
+router.get('/login', auth.login);
+
+// route for login action
+router.post('/login', auth.doLogin);
+
+// route for logout action
+router.get('/logout', auth.logout);
+
 
 module.exports = router;
